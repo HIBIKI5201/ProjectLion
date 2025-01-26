a61a9568b1ae977779e090b534c220e3ca247cad
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2025-01-26 13:35:19 +0900

    [fix]iconの変更 iconはドライブに投げる

--- Assets/Scripts/UserInterface/Ingame/StatusUI.cs
+++ Assets/Scripts/UserInterface/Ingame/StatusUI.cs
@@ -14,6 +14,10 @@ public partial class StatusUI : UIElement_B
 
     VisualElement _icon;
     Label _text;
+
+    //ToDo:‰¼
+    Texture2D _iconTexture;
+    float _InitText;
     public StatusUI() : base("UXML/StatusElement") { }
 
     protected override Task Initialize_S(TemplateContainer container)
@@ -21,13 +25,14 @@ public partial class StatusUI : UIElement_B
         _icon = container.Q<VisualElement>("Icon");
         _text = container.Q<Label>("Text");
 
-        SetIcon(_defaltIcon, _defaltColor);
-        OnStatusChanged(_defaltData);
+        SetIcon(_iconTexture, Color.white);
+        OnStatusChanged(_InitText);
 
         return Task.CompletedTask;
     }
     public void SetIcon(Texture2D icon,Color color)
     {
+        _iconTexture = icon;
         if (_icon != null)
         {
             _icon.style.backgroundImage = icon;
@@ -36,6 +41,7 @@ public partial class StatusUI : UIElement_B
     }
     public void OnStatusChanged(float status)
     {
+        _InitText = status;
         if (_text != null)
         {
             _text.text = status.ToString();
