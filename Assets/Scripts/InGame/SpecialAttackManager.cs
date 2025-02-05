5324a190168ca33a30b6ba3d9c072aa69f38712d
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2025-02-05 18:37:33 +0900

    [update]必殺技をUIと結びつける部分を作成しました。 ゲージ部分は未反映です。

--- Assets/Scripts/InGame/SpecialAttackManager.cs
+++ Assets/Scripts/InGame/SpecialAttackManager.cs
@@ -39,6 +39,7 @@ public class SpecialAttackManager : MonoBehaviour
     public void AddSpecialExperiance(float point)
     {
         _specialExperiancePoint += point;
+        Debug.Log("_specialExperiancePoint : " +_specialExperiancePoint+ " :SpecialRequirePoint :"+SpecialRequirePoint + "\nnormalize : "+(float)SpecialExperiancePoint / SpecialRequirePoint);
         SpecialEvant?.Invoke(_specialRequirePoint - _specialExperiancePoint);
         if (!_specialReady && _specialRequirePoint <= _specialExperiancePoint)
         {
@@ -57,8 +58,8 @@ public class SpecialAttackManager : MonoBehaviour
             _specialObj.SetActive(false);
             //•KŽE‹Z”­“®I—¹‚Ìˆ—
             _specialReady = false;
-            SpecialEvant?.Invoke(0);
             _specialExperiancePoint = 0;
+            SpecialEvant?.Invoke(0);
         }
     }
 }
--- Assets/Scripts/UserInterface/Ingame/InGameUIManager.cs
+++ Assets/Scripts/UserInterface/Ingame/InGameUIManager.cs
@@ -1,7 +1,9 @@
 using SymphonyFrameWork.CoreSystem;
 using System.Threading.Tasks;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEngine.Rendering.DebugUI;
 
 public class InGameUIManager : MonoBehaviour
 {
@@ -12,6 +14,7 @@ public class InGameUIManager : MonoBehaviour
     [SerializeField] LevelUpManager _levelUpManager;
     [SerializeField] LevelContainer _levelContainer;
     [SerializeField] InGameManager _inGameManager;
+    [SerializeField] SpecialAttackManager _specialAttackManager;
     PlayerController _playerController;
     private async void Start()
     {
@@ -30,27 +33,46 @@ public class InGameUIManager : MonoBehaviour
     async Task PanelSetting()
     {
         LevelUpPanel panel = _root.Q<LevelUpPanel>("LevelUpPanel");
-        await panel.InitializeTask;
-        if (panel != null)
-        {
-            _levelUpManager.OnLevelChanged += (x, y) => panel.OnLevelUp(x, y, _uiDatas);
-        }
-        else
-        {
-            Debug.Log("panel is null");
-        }
-
         TimerUI timerText = _root.Q<TimerUI>("TimerUI");
-        await timerText.InitializeTask;
-        _inGameManager.OnTimerChanged += x => timerText.OnTimerChanged(x / 60, x % 60);
-
         StatusUIManager status = _root.Q<StatusUIManager>("StatusUIManager");
-        await status.InitializeTask;
-        if (status is null) Debug.Log("statusUIManager is null");
-        _levelUpManager.OnGetItem += () => status?.OnStatusChange(LevelUpManager.ItemHaveValue);
-
+        SkillUI skill = _root.Q<SkillUI>("SkillUI");
         RewardUIManager reward = _root.Q<RewardUIManager>("RewardUI");
-        await reward.InitializeTask;
+
+        Task[] wait = new Task[] 
+        {
+            panel.InitializeTask,
+            timerText.InitializeTask,
+            status.InitializeTask,
+            skill.InitializeTask,
+            reward.InitializeTask,
+        };
+
+        await Task.WhenAll(wait);
+
+        _levelUpManager.OnLevelChanged += (x, y) => panel.OnLevelUp(x, y, _uiDatas);
+        _inGameManager.OnTimerChanged += x => timerText.OnTimerChanged(x / 60, x % 60);
+        _levelUpManager.OnGetItem += () => status?.OnStatusChange(LevelUpManager.ItemHaveValue);
+        _specialAttackManager.SpecialEvant += x => skill.ChangeSkillGage(
+            _specialAttackManager.SpecialExperiancePoint,
+            _specialAttackManager.SpecialRequirePoint);
+        skill.OnUseUltimate += () => _specialAttackManager.SpecialAttack();
         _levelContainer.OnAddExperiance += (x, y) => reward.ChangeEXP(x, y);
+    }
+    private void OnDisable()
+    {
+        LevelUpPanel panel = _root.Q<LevelUpPanel>("LevelUpPanel");
+        TimerUI timerText = _root.Q<TimerUI>("TimerUI");
+        StatusUIManager status = _root.Q<StatusUIManager>("StatusUIManager");
+        SkillUI skill = _root.Q<SkillUI>("");
+        RewardUIManager reward = _root.Q<RewardUIManager>("RewardUI");
+
+        _levelUpManager.OnLevelChanged -= (x, y) => panel.OnLevelUp(x, y, _uiDatas);
+        _inGameManager.OnTimerChanged -= x => timerText.OnTimerChanged(x / 60, x % 60);
+        _levelUpManager.OnGetItem -= () => status?.OnStatusChange(LevelUpManager.ItemHaveValue);
+        //_specialAttackManager.SpecialEvant -= x => skill.ChangeSkillGage(
+        //    _specialAttackManager.SpecialExperiancePoint,
+        //    _specialAttackManager.SpecialRequirePoint);
+        skill.OnUseUltimate -= () => _specialAttackManager.SpecialAttack();
+        _levelContainer.OnAddExperiance -= (x, y) => reward.ChangeEXP(x, y);
     }
 }
