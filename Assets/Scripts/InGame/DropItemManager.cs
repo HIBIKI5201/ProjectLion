ef8f5e9964b02ad8705d7eb74ceb0060c4450079
Oriku0308 <O.riku8863@gmail.com>
2025-03-14 00:51:56 +0900

    <Add> アイテムをドロップさせる仕組みを作り、ある程度出現率もいじれるような作りにした まだアイテム自体の制作には取り掛かっていないが、オブジェクトとそれにアタッチするスクリプトは作った

---
+++ Assets/Scripts/InGame/DropItemManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DropItemManager : MonoBehaviour
+{
+    public void Initialize()
+    {
+        //Debug.Log("DropItemManager.Initialize");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+}
