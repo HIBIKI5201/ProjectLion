74205febf9cb77a9c989bc17b04da8ad8911973f
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2025-03-26 21:00:19 +0900

    [fix]シーンローダー修正

--- Assets/Scripts/System/SceneLoader.cs
+++ Assets/Scripts/System/SceneLoader.cs
@@ -17,31 +17,58 @@ public class SceneLoader : MonoBehaviour
 
     public static async Task LoadSceneAsync(SceneKind kind)
     {
-        Scene activeScene = SceneManager.GetSceneByName(_sceneNames[kind]);
-
-        if (activeScene.isLoaded)
-        {
-            var ta = SceneManager.UnloadSceneAsync(activeScene);
-        }
+        Scene activeScene = SceneManager.GetActiveScene();//現在のmainSceneを取得
+        var task = SceneManager.LoadSceneAsync(_sceneNames[kind], LoadSceneMode.Additive);//Sceneをロード
         
-        var task = SceneManager.LoadSceneAsync(activeScene.name, LoadSceneMode.Additive);
-        task.allowSceneActivation = false;
-        Debug.Log($"Loading Scene: {activeScene.name}");
-        while (!task.isDone)
+        Debug.Log($"Active Scene: {SceneManager.GetActiveScene().name}");
+        //ロード待機
+        task.allowSceneActivation = false;//シーンを一時的に無効化
+        while (task.progress < 0.9f)
         {
-            if (task.progress >= 0.9f)
-            {
-                task.allowSceneActivation = true; // シーンをアクティブにする
-            }
+            Debug.Log(task.progress);
             await Task.Yield();
         }
-        activeScene = SceneManager.GetSceneByName(_sceneNames[kind]);
-        SceneManager.SetActiveScene(activeScene);
-        if (_currentScene.name is not null)
+        Debug.Log($"Active Scene: {SceneManager.GetActiveScene().name}");
+        Scene newScene = SceneManager.GetSceneByName(_sceneNames[kind]);
+        Debug.Log($"Loading Scene: {newScene.name}");
+        
+        task.allowSceneActivation = true;//シーンを一時的に無効化
+        await task;
+        //アクティブなシーンの変更
+        Debug.Log($"Loading Scene: {newScene.name} Unload:{activeScene.name}");
+        Debug.Log($"Active Scene: {SceneManager.GetActiveScene().name}");
+        SceneManager.SetActiveScene(newScene);
+        //前回のシーンの解放
+        var unlodeTask = SceneManager.UnloadSceneAsync(activeScene);
+
+        _currentScene = activeScene;
+    }
+    
+    public static async Task LoadSceneAsync(string str)
+    {
+        Scene activeScene = SceneManager.GetActiveScene();//現在のmainSceneを取得
+        var task = SceneManager.LoadSceneAsync(str, LoadSceneMode.Additive);//Sceneをロード
+        
+        Debug.Log($"Active Scene: {SceneManager.GetActiveScene().name}");
+        //ロード待機
+        task.allowSceneActivation = false;//シーンを一時的に無効化
+        while (task.progress < 0.9f)
         {
-            task = SceneManager.UnloadSceneAsync(_currentScene.name);
-            await task;
+            Debug.Log(task.progress);
+            await Task.Yield();
         }
+        Debug.Log($"Active Scene: {SceneManager.GetActiveScene().name}");
+        Scene newScene = SceneManager.GetSceneByName(str);
+        Debug.Log($"Loading Scene: {newScene.name}");
+        
+        task.allowSceneActivation = true;//シーンを一時的に無効化
+        await task;
+        //アクティブなシーンの変更
+        Debug.Log($"Loading Scene: {newScene.name} Unload:{activeScene.name}");
+        Debug.Log($"Active Scene: {SceneManager.GetActiveScene().name}");
+        SceneManager.SetActiveScene(newScene);
+        //前回のシーンの解放
+        var unlodeTask = SceneManager.UnloadSceneAsync(activeScene);
 
         _currentScene = activeScene;
     }
