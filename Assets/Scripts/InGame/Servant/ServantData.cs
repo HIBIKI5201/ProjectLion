9234301d32b6eba4294ed9b5c329335f818fa2d0
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2025-03-28 02:35:58 +0900

    [fix]UIのScriptableObjectの位置を変更しました。

--- Assets/Scripts/InGame/Servant/ServantData.cs
+++ Assets/Scripts/InGame/Servant/ServantData.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ServantData",menuName = "GameData/ServantData")]
 public class ServantData : MobData_S
 {
-    [SerializeField,Space] float _moveStopDistanse;
+    [SerializeField,Space,Header("サーバント固有設定")] float _moveStopDistanse;
     [SerializeField,Tooltip("使い魔のレベルが上がった時に自動的に上がるステータス倍率 nameステータスは設定しなくても大丈夫です")]
     MobData _levelScaleData;
     public float MoveStopDistanse => _moveStopDistanse;
