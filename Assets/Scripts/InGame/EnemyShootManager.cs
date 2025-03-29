df83e9162730cc38ec0fd76766279d18fff87b9a
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2025-03-30 01:13:18 +0900

    [fix]コンフリクトしたためmain側の変更に戻しました。

--- Assets/Scripts/InGame/EnemyShootManager.cs
+++ Assets/Scripts/InGame/EnemyShootManager.cs
@@ -39,38 +39,37 @@ public class EnemyShootManager : MonoBehaviour, PauseManager.IPausable
         else if (_timer + _interval < Time.time)
         {
             _timer = Time.time;
+
+            if (!_playerController) return;//プレイヤーが存在しないとき
             Shoot();
         }
     }
 
     private async void Shoot()
     {
-        if (_playerController)
+        Vector2 direction = (_playerController.transform.position - _enemyManager.transform.position).normalized;
+        AsyncInstantiateOperation operation = InstantiateAsync(_bullet, transform.position, Quaternion.Euler(direction));
+        await operation;
+        foreach (GameObject obj in operation.Result)
         {
-            Vector2 direction = (_playerController.transform.position - _enemyManager.transform.position).normalized;
-            AsyncInstantiateOperation operation = InstantiateAsync(_bullet, transform.position, Quaternion.Euler(direction));
-            await operation;
-            foreach (GameObject obj in operation.Result)
+
+            if (!obj.TryGetComponent<BulletManager>(out var mg))
+                mg = obj.AddComponent<BulletManager>();
+
+            mg.SetStatus(_enemyManager.Attack);
+
+            if (obj.TryGetComponent<Rigidbody2D>(out var rb))
             {
-
-                if (!obj.TryGetComponent<BulletManager>(out var mg))
-                    mg = obj.AddComponent<BulletManager>();
-
-                mg.SetStatus(_enemyManager.Attack);
-
-                if (obj.TryGetComponent<Rigidbody2D>(out var rb))
-                {
-                    rb.gravityScale = 0;
-                    rb.linearDamping = 0;
-                    rb.linearVelocity = direction * _bulletSpeed;
-                }
-                if (obj.TryGetComponent<CircleCollider2D>(out var cc))
-                {
-                    cc.isTrigger = true;
-                }
-                await PauseManager.PausableWaitForSecondAsync(_bulletDuration);
-                Destroy(obj);
+                rb.gravityScale = 0;
+                rb.linearDamping = 0;
+                rb.linearVelocity = direction * _bulletSpeed;
             }
+            if (obj.TryGetComponent<CircleCollider2D>(out var cc))
+            {
+                cc.isTrigger = true;
+            }
+            await PauseManager.PausableWaitForSecondAsync(_bulletDuration);
+            Destroy(obj);
         }
     }
 
@@ -109,7 +108,7 @@ public class EnemyShootManager : MonoBehaviour, PauseManager.IPausable
         }
         public void Pause()
         {
-            if( _rb != null ) 
+            if (_rb != null)
                 _rb.simulated = false;
             Debug.Log(_rb.ToString());
         }
